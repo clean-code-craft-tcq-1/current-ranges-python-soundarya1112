490d2ae879f0ea7ba355a6e44a6fe96c58e41239
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-05-06 14:37:05 +0530

    Update bms_current_test.py

--- bms_current_test.py
+++ bms_current_test.py
@@ -1,14 +1,14 @@
 import unittest
 import bms_current_ranges
 
-class test_battery_current_ranges(unittest.TestCase):
+class test_empty_current_ranges(unittest.TestCase):
   def test_failing_current_ranges(self):
     current_input = []
     self.assertTrue(bms_current_ranges.check_empty_input(current_input) == "Empty_Input")
     
-  def test_passing_current_ranges(self):
-    current_valid_input = [4,3,2,1]
-    self.assertEqual((bms_current_ranges.check_is_nan(current_valid_input) == "Valid Input")
+  def test_is_nan_current_ranges(self):
+    current_valid_input = [4,3,math.nan,1]
+    self.assertEqual(bms_current_ranges.check_is_nan(current_valid_input) == "Invalid Input")
     
     
 if __name__ == '__main__':
