3833ac036d190428ef9f48707426c0551b686bce
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-05-06 12:02:04 +0530

    Update bms_current_ranges.py

--- bms_current_ranges.py
+++ bms_current_ranges.py
@@ -1,5 +1,13 @@
+import numpy as np
+
 def check_valid_input(current_ranges):
     if len(current_ranges) == 0:
         return 'Invalid_Input'
     return True
+
+def check_is_nan(current_rages)
+    if np.isnan(current_rages):
+        return True
+    else:
+        return False
   
